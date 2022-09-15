9c1c51f23780d9cceab6661c358ee11593851d81
Krzysztof Momot <krz.momot@gmail.com>
2022-09-15 13:16:13 +0200

    Add test data

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO "credit_card" ("id", "number", "limit") VALUES (1, '123-456-789-012', 100)
+
+INSERT INTO "item" ("id", "cost") VALUES ('TV', 1000)
+INSERT INTO "item" ("id", "cost") VALUES ('USB', 10)
